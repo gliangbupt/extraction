bb869a95953cb2209993712aad91904c00a5c4cf
gliangbupt <gliangjlu@163.com>
2018-11-15 15:45:34 +0800

    add symbol

--- extraction.py
+++ extraction.py
@@ -3,8 +3,8 @@ import re
 
 
 
-def name_split(pathname,symbol):            #提取名称信息（包括光源和强度）
-    # symbol=[_-]
+def name_split(pathname):            #提取名称信息（包括光源和强度）
+    symbol="[ _-]"
 
     [path,f_name]=os.path.split(pathname)   #将路径与名称分隔开保存为Tuple
     pre_result=re.split(symbol,f_name)      #利用正则表达式分隔开名称
